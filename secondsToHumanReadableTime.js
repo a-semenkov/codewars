e497de8c105469a42e45033facd4c7ed86700b95
Andrey Semenkov <asemenkov@gmail.com>
2023-03-15 16:25:32 +0300

    solved

--- secondsToHumanReadableTime.js
+++ secondsToHumanReadableTime.js
@@ -19,5 +19,15 @@ function humanReadable(seconds) {
   }`;
 }
 
-v = humanReadable(35);
-v;
+function humanReadable(seconds) {
+  const pad = function (x) {
+    return x < 10 ? '0' + x : x;
+  };
+  return (
+    pad(parseInt(seconds / (60 * 60))) +
+    ':' +
+    pad(parseInt((seconds / 60) % 60)) +
+    ':' +
+    pad(seconds % 60)
+  );
+}
